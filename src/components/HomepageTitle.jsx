459f729dfd20f340affd0de18777049d91550304
Hajin Park <hajin1819@gmail.com>
2022-10-26 07:45:31 -0700

    Formatting and styling updates

--- src/components/HomepageTitle.jsx
+++ src/components/HomepageTitle.jsx
@@ -1,7 +1,7 @@
 export default function HomepageTitle() {
     return (
         <header>
-            <h1 className="text-moss-500 text-8xl font-bold">Ucentials marketplace</h1>
+            <h1 className="text-center text-transparent bg-clip-text bg-gradient-to-r from-moss-700 to-moss-500 text-8xl font-bold">Ucentials marketplace</h1>
         </header>
     )
 }
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,7 +1,7 @@
 export default function NavigationBar() {
     return (
-        <nav className="flex w-full h-32">
-            <ul className="flex w-full h-full">
+        <nav className="w-full h-16">
+            <ul className="flex items-end justify-around w-full h-full border-b-white border-b-[1px]">
                 <li className="text-4xl font-semibold text-moss-500">Home</li>
                 <li className="text-4xl font-semibold text-moss-500">About</li>
                 <li className="text-4xl font-semibold text-moss-500">Contact</li>
