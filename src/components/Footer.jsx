2d0c492bdbaa887be5ad6fec666f15abec0f39ed
Hajin Park <hajin1819@gmail.com>
2022-10-29 00:24:44 -0700

    Updates idk

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer>
             <address>
-                <ul className="flex justify-around border-t-[1px] border-t-white">
+                <ul className="flex justify-around border-t-[1px] border-t-black">
                     <li>Email</li>
                     <li>Instagram</li>
                     <li>Discord</li>
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,11 +1,11 @@
 export default function NavigationBar() {
     return (
         <nav className="w-full h-16">
-            <ul className="flex items-end justify-around w-full h-full border-b-white border-b-[1px]">
-                <li className="text-4xl font-semibold text-moss-500">Home</li>
-                <li className="text-4xl font-semibold text-moss-500">About</li>
-                <li className="text-4xl font-semibold text-moss-500">Contact</li>
-                <li className="text-4xl font-semibold text-moss-500">Login/Sign-up</li>
+            <ul className="flex items-end justify-around w-full h-full border-b-black border-b-[1px]">
+                <li className="text-4xl font-semibold">Home</li>
+                <li className="text-4xl font-semibold">About</li>
+                <li className="text-4xl font-semibold">Contact</li>
+                <li className="text-4xl font-semibold">Login/Sign-up</li>
             </ul>
         </nav>
     )
