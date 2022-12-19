ec2e37115132baef2fabc58147d3f5e9d87a8df4
Hajin Park <hajin1819@gmail.com>
2022-12-19 01:34:43 -0800

    Wireframe test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,17 @@
-import Filler from '../components/Filler'
+import ProductButton from '../components/ProductButton'
 
 export default function HomePage() {
     return (
-        <>
-            <Filler />
-        </>
+        <article className="w-full grid-cols-1">
+            <h1 className="text-center self-center font-poppins bg-gradient-to-tr from-lime-100 to-lime-600 bg-clip-text text-transparent text-7xl md:text-9xl font-black py-56">Ucentials</h1>
+            <nav className="grid grid-cols-1 md:grid-cols-2 px-24 md:px-32 lg:px-44 xl:px-56 pb-12 gap-12">
+                <ProductButton name="Student Marketplace"/>
+                <ProductButton name="Major Course Planner"/>
+                <ProductButton name="Campus Directory"/>
+                <ProductButton name="Dining Hall Menu"/>
+                <ProductButton name="..."/>
+                <ProductButton name="..."/>
+            </nav>
+        </article>
     )
 }
